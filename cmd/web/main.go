a78dacc03c3a4529591d3b7e3c10142449bf87ee
appletreecy <appletreecy@gmail.com>
2025-01-03 15:45:12 +1100

    completed section 12

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,14 +1,18 @@
 package main
 
 import (
+	"encoding/gob"
 	"fmt"
 	"log"
 	"net/http"
+	"os"
 	"time"
 
 	"github.com/alexedwards/scs/v2"
+	"github.com/appletreecy/golang_web/helpers"
 	"github.com/appletreecy/golang_web/internal/config"
 	"github.com/appletreecy/golang_web/internal/handlers"
+	"github.com/appletreecy/golang_web/internal/models"
 	"github.com/appletreecy/golang_web/internal/render"
 )
 
@@ -18,35 +22,16 @@ var app config.AppConfig
 
 var session *scs.SessionManager
 
+var infoLog *log.Logger
+var errorLog *log.Logger
+
 // main is the main application function
 func main() {
 
-	// change this to true when in production
-	app.InProduction = false
-
-	session = scs.New()
-	session.Lifetime = 24 * time.Hour
-	session.Cookie.Persist = true
-	session.Cookie.SameSite = http.SameSiteLaxMode
-	session.Cookie.Secure = app.InProduction
-
-	app.Session = session
-
-	tc, err := render.CreateTemplateCache()
+	err := run()
 	if err != nil {
-		log.Fatal("cannot create template cache")
+		log.Fatal(err)
 	}
-
-	app.TemplateCache = tc
-	app.UseCache = false
-
-	repo := handlers.NewRepo(&app)
-	handlers.NewHandlers(repo)
-
-	render.NewTemplates(&app)
-
-	// http.HandleFunc("/", handlers.Repo.Home)
-	// http.HandleFunc("/about", handlers.Repo.About)
 
 	fmt.Println(fmt.Sprintf("Staring application on port %s", portNumber))
 	// _ = http.ListenAndServe(portNumber, nil)
@@ -60,4 +45,44 @@ func main() {
 	if err != nil {
 		log.Fatal(err)
 	}
+}
+
+func run() error {
+
+	// what am I going to put in the session
+	gob.Register(models.Reservation{})
+
+	// change this to true when in production
+	app.InProduction = false
+
+	infoLog = log.New(os.Stdout, "INFO\t", log.Ldate|log.Ltime)
+	app.InfoLog = infoLog
+
+	errorLog = log.New(os.Stdout, "ERROR\t", log.Ldate|log.Ltime|log.Lshortfile)
+	app.ErrorLog = errorLog
+
+	session = scs.New()
+	session.Lifetime = 24 * time.Hour
+	session.Cookie.Persist = true
+	session.Cookie.SameSite = http.SameSiteLaxMode
+	session.Cookie.Secure = app.InProduction
+
+	app.Session = session
+
+	tc, err := render.CreateTemplateCache()
+	if err != nil {
+		log.Fatal("cannot create template cache")
+		return err
+	}
+
+	app.TemplateCache = tc
+	app.UseCache = false
+
+	repo := handlers.NewRepo(&app)
+	handlers.NewHandlers(repo)
+
+	render.NewTemplates(&app)
+	helpers.NewHelpers(&app)
+
+	return nil
 }
---
+++ internal/forms/forms_test.go
@@ -0,0 +1,107 @@
+package forms
+
+import (
+	"net/http"
+	"net/http/httptest"
+	"net/url"
+	"testing"
+)
+
+func TestForm_Valid(t *testing.T) {
+	r := httptest.NewRequest("POST", "/whatever", nil)
+	form := New(r.PostForm)
+
+	isValid := form.Valid()
+	if !isValid {
+		t.Error("got invalid when should have been valid")
+	}
+}
+
+func TestForm_Required(t *testing.T) {
+	r := httptest.NewRequest("POST", "/whatever", nil)
+	form := New(r.PostForm)
+
+	form.Required("a", "b", "c")
+	if form.Valid() {
+		t.Error("form shows valid when required fields missing")
+	}
+
+	postedData := url.Values{}
+	postedData.Add("a", "a")
+	postedData.Add("b", "a")
+	postedData.Add("c", "a")
+
+	r, _ = http.NewRequest("POST", "/whatever", nil)
+
+	r.PostForm = postedData
+	form = New(r.PostForm)
+	form.Required("a", "b", "c")
+	if !form.Valid() {
+		t.Error("shows does not have required fields when it does.")
+	}
+
+}
+
+func TestForm_Has(t *testing.T) {
+	r := httptest.NewRequest("POST", "/whatever", nil)
+	form := New(r.PostForm)
+
+	has := form.Has("whatever")
+	if has {
+		t.Error("form shows has field when it does not.")
+	}
+
+	postedData := url.Values{}
+	postedData.Add("a", "a")
+	form = New(postedData)
+
+	has = form.Has("a")
+	if !has {
+		t.Error("shows form does not have field when it should")
+	}
+}
+
+func TestForm_MinLength(t *testing.T) {
+
+	r := httptest.NewRequest("POST", "/whatever", nil)
+	form := New(r.PostForm)
+
+	postedData := url.Values{}
+	postedData.Add("a", "steven")
+	form = New(postedData)
+
+	result := form.MinLength("a", 3)
+
+	if !result {
+		t.Error("Min length not met")
+	}
+
+}
+
+func TestForm_IsEmail(t *testing.T) {
+	postedValues := url.Values{}
+	form := New(postedValues)
+
+	form.IsEmail("x")
+	if form.Valid() {
+		t.Error("forms show valid email for non-existent field")
+	}
+
+	postedValues = url.Values{}
+	postedValues.Add("email", "me@here.com")
+	form = New(postedValues)
+
+	form.IsEmail("email")
+	if !form.Valid() {
+		t.Error("got an invalid email when whe should not have")
+	}
+
+	postedValues = url.Values{}
+	postedValues.Add("email", "x")
+	form = New(postedValues)
+
+	form.IsEmail("email")
+	if form.Valid() {
+		t.Error("got valid for invalid email address.")
+	}
+}
